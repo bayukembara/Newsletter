f3ac25843b60420b75230d143a445404622eb752
Bayu K <bayukembara@gmail.com>
2022-10-15 21:10:43 +0700

    Newsletter

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  // Setting for requesting
+  const jsonData = JSON.stringify(data);
+  const url = "https://us21.api.mailchimp.com/3.0/lists/50ce8aa7e5";
+  const options = {
+    method: "POST",
+    auth: "bayu1:82e1ef0f09794530389b00614408d6a9-us21",
+  };
+
+  // Request API and do what you want it
+  const request = https.request(url, options, (response) => {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+  // from request we get, we want to create in API, and the fill is in jsonData
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("This Server is Running");
+});
+
+// Chimp Api Key :82e1ef0f09794530389b00614408d6a9-us21
+// Unique Id for audience :50ce8aa7e5
